63404ffa1c96be9aa0c2406ef1099f380fb49ed7
Yin <yinnnn17@gmail.com>
2020-10-03 19:16:35 -0700

    add rot13 cipher

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,11 @@
 // ******caesar_cipher**********************************************
 const caesar_cipher = document.getElementById('Caesar');
 const caesar_cipher_msg_input = document.getElementById('caesar-msg');
+// When ever user change anything in the form, update result
 caesar_cipher.onchange = function (e) {
-	processResult();
+	processResultCaesar();
 };
-//
+//------ Decode/ Encode Messages -----------
 function isUpperCase(str) {
 	//check if the letter is uppercase
 	return str === str.toUpperCase();
@@ -14,7 +15,7 @@ function isSpace(str) {
 	return str === ' ';
 }
 // Handle Encoding
-function ceaserCipherEncode(str, key) {
+function encodeByShift(str, key) {
 	let decipher = '';
 	for (let i = 0; i < str.length; i++) {
 		if (isSpace(str[i])) {
@@ -36,7 +37,7 @@ function ceaserCipherEncode(str, key) {
 	return decipher;
 }
 // Handle Decoding
-let ceaserCipherDecode = (str, key) => {
+let decodeByShift = (str, key) => {
 	let decipher = '';
 	for (let i = 0; i < str.length; i++) {
 		if (isSpace(str[i])) {
@@ -57,20 +58,39 @@ let ceaserCipherDecode = (str, key) => {
 	}
 	return decipher;
 };
-function processResult() {
+//-------------------------------------------------------------------
+function processResultCaesar() {
 	let msg = caesar_cipher_msg_input.value;
 	let action_input = document.getElementById('caesar-action');
 	let shift_input = document.getElementById('caesar-shift');
 	let output = document.getElementById('caesar-result');
 	let result = '';
 	if (action_input.value === 'encode') {
-		result = ceaserCipherEncode(msg, parseInt(shift_input.value));
+		result = encodeByShift(msg, parseInt(shift_input.value));
 	} else if (action_input.value === 'decode') {
-		result = ceaserCipherDecode(msg, parseInt(shift_input.value));
+		result = decodeByShift(msg, parseInt(shift_input.value));
 	}
 	output.innerHTML = result;
 }
-processResult();
-caesar_cipher_msg_input.addEventListener('input', processResult);
+processResultCaesar();
+caesar_cipher_msg_input.addEventListener('input', processResultCaesar);
 // ******Rot13 Cipher**********************************************
+const rot13 = document.getElementById('Rot');
+const rot13_msg_input = document.getElementById('rot-msg');
+const submit_rot = document.getElementById('submit-rot');
+function processResultRot() {
+	let msg = rot13_msg_input.value;
+	let action_input = document.getElementById('rot-action');
+	let output = document.getElementById('rot-result');
+	let result = '';
+	if (action_input.value === 'encode') {
+		result = encodeByShift(msg, 13);
+	} else if (action_input.value === 'decode') {
+		result = decodeByShift(msg, 13);
+	}
+	output.innerHTML = result;
+}
+processResultRot();
+console.log(submit_rot);
+submit_rot.addEventListener('click', processResultRot);
 // ******Polybius Cipher*******************************************
